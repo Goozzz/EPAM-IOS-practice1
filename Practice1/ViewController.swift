7d0477560ee934dff50f7dd58e4b2df60e63fd50
Evgeny Gusev <wackyy52@gmail.com>
2020-01-23 16:08:56 +0300

    Add logic to UI

--- Practice1/ViewController.swift
+++ Practice1/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    var gameNumber = 0
+    
+    @IBOutlet weak var userGuessTextField: UITextField!
+    @IBOutlet weak var inputNumberButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var dialogLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewGame()
+    }
+    
+    func startNewGame() {
+        resetButton.isHidden = true
+        inputNumberButton.isHidden = false
+        userGuessTextField.text = ""
+        gameNumber = Int.random(in: 1...100)
+        dialogLabel.text = "Guess the number!"
+    }
+    
+    func finishGame() {
+        inputNumberButton.isHidden = true
+        resetButton.isHidden = false
+        dialogLabel.text = "Congratulations!"
+    }
+    
+    func checkGuess(guessNumber: Int) {
+        if(guessNumber == -1) {
+            return
+        }
+        
+        if guessNumber == gameNumber {
+            finishGame()
+        } else if guessNumber >  gameNumber {
+            dialogLabel.text = "Number is less"
+        } else {
+            dialogLabel.text = "Number is greater"
+        }
     }
 
-
+    @IBAction func enterGuess(_ sender: Any) {
+        let guessNumber = Int(userGuessTextField.text!) ?? -1
+        userGuessTextField.text = ""
+        checkGuess(guessNumber: guessNumber)
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        startNewGame()
+    }
 }
+
 
