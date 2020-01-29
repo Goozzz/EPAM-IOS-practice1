188e00106b2d7de0b7cf3384e873e6afbd9a1b59
Evgeny Gusev <wackyy52@gmail.com>
2020-01-29 20:17:52 +0300

    Create controller for SettingsView

---
+++ Practice1/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  Practice1
+//
+//  Created by Евгений Гусев on 29.01.2020.
+//  Copyright © 2020 Евгений Гусев. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    var maxNumber = 100
+    var minNumber = 1
+    
+    override func viewDidLoad() {
+        
+    }
+}
